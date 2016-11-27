498f2b5c5ec1998bead2c4afbc40654d6a2a1a49
Say <120011676@qq.com>
2016-11-27 22:37:46 +0800

    实验

--- src/main/java/org/say/valuation/Application.java
+++ src/main/java/org/say/valuation/Application.java
@@ -1,5 +1,6 @@
 package org.say.valuation;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@EntityScan(basePackages="org.say.valuation.entity")
 //@EnableJpaRepositories(basePackages="org.say.valuation.dao")
 //@EnableAutoConfiguration
+@EnableAdminServer
 @SpringBootApplication
 public class Application {
 
